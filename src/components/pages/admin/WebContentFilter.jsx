bb85094bd46d5554dfac1c809dfaee497e5c3e5f
Princess Pocon <pocon.evangeline@gmail.com>
2023-04-11 20:54:58 +0800

    add web content filter table

--- src/components/pages/admin/WebContentFilter.jsx
+++ src/components/pages/admin/WebContentFilter.jsx
@@ -1,10 +1,110 @@
-import { Box ,Typography} from "@mui/material"
+import {  useState,useEffect } from 'react';
+import { Box, Container, Stack,Button,  Typography } from '@mui/material';
+import { WebContentFilterTable } from '../../sections/webContentFilter/webContentFilterTable';
+import { StudentSearch } from '../../sections/student/StudentSearch';
+import { instance } from '../../../helper/http';
+import {IoMdAdd} from 'react-icons/io';
+
 export const WebContentFilter = () => {
+  instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+  const [listOfWebContent, setlistOfWebContent] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [isUpdated, setIsUpdated] = useState(false);
 
-    return(
-        <Box>
-            <Typography variant="h4">Web Content Filter</Typography>
-        </Box>
+  
+  useEffect(() => {
+    console.log("instance", instance)
+    
+    instance.get('/api/admin/students').then((response) => {
+      console.log(response.data.students);
+      setlistOfWebContent(response.data.students);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, [isUpdated]);
 
-    )
-}
+
+  const handleSearchStudent = () => {
+    if (searchValue === '') {
+      instance.get('/api/admin/students').then((response) => {
+        console.log(response.data.students);
+        setlistOfWebContent(response.data.students);
+      }).catch((error) => {
+        console.log(error);
+      });
+      return;
+    }
+    instance.get(`/api/admin/students/search/${searchValue}`).then((response) => {
+      console.log(response.data.students);
+      setlistOfWebContent(response.data.students);
+    }).catch((error) => {
+      console.log(error);
+      if (error.response.status === 404) {
+        setlistOfWebContent([]);
+      }
+    });
+
+  }
+
+
+  const handleStudentDelete = async (e,studentId) => {
+
+    console.log(studentId);
+    await instance.delete(`/api/admin/students/${studentId}`).then((response) => {
+      console.log(response.data.message);
+      setlistOfWebContent(listOfWebContent.filter((student) => student.id !== studentId));
+      setIsUpdated(!isUpdated);
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  }
+
+  return (
+    <>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8
+        }}
+      >
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              spacing={4}
+            >
+              <Stack spacing={1}>
+                <Typography variant="h4">
+                  Web Content Filter
+                </Typography>
+                
+              </Stack>
+
+              <Button variant="contained" color="success" 
+                 >
+                      Add
+                      <IoMdAdd size={20} style={{marginLeft: '5px'}} />
+                  </Button>
+        
+            </Stack>
+            <StudentSearch onChange={(e)=> setSearchValue(e.target.value)} value={searchValue} 
+              onSearch={handleSearchStudent}
+            />
+            <WebContentFilterTable
+              count={listOfWebContent.length}
+              items={listOfWebContent}
+         
+              onDelete={handleStudentDelete}
+  
+            />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+
---
+++ src/components/sections/webContentFilter/WebContentFilterTable.jsx
@@ -0,0 +1,90 @@
+
+import {
+  Box,
+  Button,
+  Card,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography
+} from '@mui/material';
+import { error } from '../../../theme/colors';
+import { LetterAvatar } from '../../general/LetterAvatar';
+
+export const WebContentFilterTable = (props) => {
+
+  const {
+    count = 0,
+    items = [],
+    onDelete = () => {},
+  } = props;
+
+  return (
+    <Card>
+        <Box sx={{ minWidth: 800 }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+              <TableCell>
+                  
+                </TableCell>
+                <TableCell>
+                  Name
+                </TableCell>
+                <TableCell>
+                  URL
+                </TableCell>
+                <TableCell>
+                  Category
+                </TableCell>
+                <TableCell>
+
+                </TableCell>
+               
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {items.map((web,index) => {
+
+                return (
+                  <TableRow
+                    hover
+                    key={web.id}
+                  >
+                    <TableCell>
+                      {index + 1}
+                    </TableCell>
+                    <TableCell>
+                      {web.name}
+                    </TableCell>
+                    <TableCell>
+                      {web.url}
+                    </TableCell>
+                    <TableCell>
+                      {web.category}
+                    </TableCell>
+                    <TableCell>
+                      <Button variant="secondary" onClick={(e)=>onDelete(e,student.id)}>
+                        <Typography variant="subtitle2" color={error.main}>
+                          Remove
+                        </Typography>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Box>
+
+      <Table
+        component="div"
+        count={count}
+      
+      />
+    </Card>
+  );
+};
