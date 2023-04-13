2a6a29a70799a0b0141d8870ba0de76caca13567
Princess Pocon <pocon.evangeline@gmail.com>
2023-04-13 23:19:29 +0800

    add homepage and deploy command to package.json

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/captive-portal-frontend-v2/'
-  
+  base: '/'
+
   //change server port
 
 })
