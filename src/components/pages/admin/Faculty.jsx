cfd5c97c15e85e36f984b6adc6b681057aef4d1f
Princess Pocon <pocon.evangeline@gmail.com>
2023-04-14 05:36:16 +0800

    change url in student table

--- src/components/pages/admin/Faculty.jsx
+++ src/components/pages/admin/Faculty.jsx
@@ -1,7 +1,7 @@
 import {  useState,useEffect } from 'react';
 import { Box, Container, Stack,  Typography } from '@mui/material';
-import { StudentTable } from '../../sections/student/studentTable';
-import { StudentSearch } from '../../sections/student/StudentSearch';
+import { StudentTable } from './../../sections/student/studentTable';
+import { StudentSearch } from './../../sections/student/StudentSearch';
 import { instance } from '../../../helper/http';
 
 export const Faculty = () => {
